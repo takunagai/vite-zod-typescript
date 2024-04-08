f905c0e64e0da024559ebcd0d144b8b45a209a88
takna <tak_na@icloud.com>
2024-04-08 16:53:50 +0900

    Zodライブラリをインポートし、ユーザースキーマを設定
    Zodライブラリを利用して、usernameプロパティを持つユーザースキーマを設定しました。この変更により、ユーザーオブジェクトのバリデーションが可能となります。

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
+import { z } from 'zod'
 import './style.css'
+
+const UserSchema = z.object({
+    username: z.string(),
+})
+
+const user = { username: 'John' }
+
+const result = UserSchema.parse(user) // 数値だとエラーが throw される
+console.log(result)
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h1>TypeScriptでのZodの利用</h1>
     <p>
-      Vite + TypeScript + Zod
+      Vite + TypeScript + Zod<br />
+      コンソールを確認してください
     </p>
   </div>
 `
